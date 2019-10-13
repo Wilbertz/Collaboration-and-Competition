d2cf66d8138da3b2ed0a963a4a2e72b3f8eb037d
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-10-13 13:55:52 +0200

    Soft update added.

--- agent.py
+++ agent.py
@@ -68,6 +68,17 @@ class Agent:
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
 
+    def soft_update(self, local_model, target_model, tau):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+        Params
+        ======
+            local_model: PyTorch model (weights will be copied from)
+            target_model: PyTorch model (weights will be copied to)
+            tau (float): interpolation parameter
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
 
 class OrnsteinUhlenbeckNoise:
     """ Ornstein-Uhlenbeck process. The process is a stationary Gauss–Markov process,
