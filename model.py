366828827adb1771a2694e4b961b15fa8fae95c6
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-10-26 15:33:05 +0200

    Comment added for model.

--- model.py
+++ model.py
@@ -26,14 +26,14 @@ class Actor(nn.Module):
     """Actor (Policy) Model."""
 
     def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=64):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
+        """
+            Initialize fields and build the model.
+            Args:
+                state_size (int): Dimension of each state
+                action_size (int): Dimension of each action
+                seed (int): Random seed
+                fc1_units (int): Number of nodes in first hidden layer
+                fc2_units (int): Number of nodes in second hidden layer
         """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
@@ -43,12 +43,21 @@ class Actor(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+        """
+           Reset the nodes weights.
+        """
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
-        """Build an actor (policy) network that maps states -> actions."""
+        """
+            Forward propagation of input in order to predict actions.
+            Args:
+                state (PyTorch model): The observed state used to predict actions
+            Returns:
+                An action vector.
+        """
         x = f.relu(self.fc1(state))
         x = f.relu(self.fc2(x))
         return f.tanh(self.fc3(x))
@@ -58,14 +67,14 @@ class Critic(nn.Module):
     """Critic (Value) Model."""
 
     def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=64):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fcs1_units (int): Number of nodes in the first hidden layer
-            fc2_units (int): Number of nodes in the second hidden layer
+        """
+            Initialize parameters and build model. T
+            Args:
+                state_size (int): Dimension of each state
+                action_size (int): Dimension of each action
+                seed (int): Random seed
+                fcs1_units (int): Number of nodes in first hidden layer
+                fc2_units (int): Number of nodes in second hidden layer
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
@@ -75,12 +84,22 @@ class Critic(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+        """
+            Reset the nodes weights.
+        """
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
-        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        """
+            Forward propagation of input in order to predict actions.
+            Args:
+                state (PyTorch model): The observed state used to predict q values.
+                action (PyTorch model): The selected action used to predict q values.
+            Returns:
+                An single q value.
+        """
         xs = f.relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
         x = f.relu(self.fc2(x))
